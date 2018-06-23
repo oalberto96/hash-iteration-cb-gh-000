c18f3faff415f42d4289f90796da294ba8df42b3
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 21:09:09 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,6 +8,7 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
   passengers.each do |key, value|
     if key == :suite_a && value.start_with?("A")
       winner = value
