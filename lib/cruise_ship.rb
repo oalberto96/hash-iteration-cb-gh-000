42a7320073b8df6ceceb211ac560567e8db9735f
Alberto Ochoa <ochoapublic@gmail.com>
2018-06-23 21:06:47 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -11,5 +11,6 @@ def select_winner(passengers)
   passengers.each do |key, value|
     if key == :suite_a && value.start_with?("A")
       winner = value
+    end
   end
 end
